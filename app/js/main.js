34aa09fd2583ebd47aa976b8dac4429d6110b500
Aleksey Erofeyev <punkjeki@yandex.ru>
2015-11-01 03:30:50 +0300

    Слайдер

--- app/js/main.js
+++ app/js/main.js
@@ -1,32 +1,45 @@
 $( document ).ready(function() {
     $('.info p').columnize({ columns: 2 });
-var Accordion =  (function ( ) {														//Замена Моего Фото, думаю пояснений не требует
+var Accordion =  (function ( ) {
 	var show = function () {
 			$('.filter_holder').hide();
-			$ ('.filter_name').on('click',function (e) {
-				if(!$(this).hasClass('open')){
+			$('.filter_name').on('click',function (e) {
+			if(!$(this).hasClass('open')){
 			 $(this).siblings('.filter_holder').slideDown('400');
-			 $(this).addClass('open');
-			 Accordion.hide();
+			 $(this).addClass('open');				
+			} else {
+			$(this).siblings('.filter_holder').slideUp('400');
+			$(this).removeClass('open');
 			}
-			})   
-	    };	
-
-	    var hide = function () {
-			$ ('.filter_name').on('click',function (e) {	    	
-	    	if($(this).hasClass('open')){	
-	    	$(this).removeClass('open');    		
-			 $(this).siblings('.filter_holder').slideUp('400');
-	    	}
 			})   
 	    };		
 		
     return {
-    	hide : hide,
     	show : show
 };
 }());
 
 Accordion.show();
 
+var Slider =  (function ( ) {
+	var slide = function  () {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+      slide: function( event, ui ){
+        $( ".amount_min" ).val( "$" + ui.values[ 0 ]);
+        $( ".amount_max" ).val( "$" + ui.values[ 1 ]);
+      }
+    });
+    $( ".amount_min" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ));
+    $( ".amount_max" ).val( "$" + $( "#slider-range" ).slider( "values", 1 ));
+	}	
+		
+    return {
+    	slide : slide
+};
+}());
+Slider.slide()
 });
